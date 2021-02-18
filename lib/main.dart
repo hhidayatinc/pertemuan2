f5abd950dc24de887088e44d66f5ab3c7983c011
Hidayati Nur Chasanah <chasanah55@gmail.com>
2021-02-18 20:54:55 +0700

    Kelanjutan

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
      home: Scaffold(
        appBar: AppBar(title: Text("MyApp"), backgroundColor: Colors.red,),
-       body: Column(
+       body: ListView(
          children: [
            Container (
              child: Row(
@@ -39,9 +39,9 @@ class MyApp extends StatelessWidget {
                ],),
              ),
            Container(
-             child: Image(image: NetworkImage('https://asset.kompas.com/crops/UZww9iUgQ6Ia2cSqFdD4e5lwYxE=/180x90:1260x810/750x500/data/photo/2019/10/16/5da6b4a19445d.png'),
+             child: Image(image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2019/05/30/83f81adb-4461-48c0-bdd9-c7382ea91b20_169.jpeg?w=700&q=90'),
              ),
-          ),
+           ),
            Container(
              color: Colors.white,
              child: Text(
@@ -61,7 +61,100 @@ class MyApp extends StatelessWidget {
              height: 45.0,
              width: 400.0,
              alignment: Alignment.centerLeft,
-           )
+           ),
+           Container(
+             child: Row(children: [
+               Container(
+                 child: Image(image: NetworkImage('https://cdn.medcom.id/dynamic/content/2020/07/20/1166802/7QLnNcGsE0.jpg?w=700'),
+                 ),
+                 width: 160.0,
+                 //height: 80.0,
+               ),
+               Container(
+                 color : Colors.white,
+                 child : Text(
+                   'Pique Bilang Wasit Untungkan Madrid, Koeman  Tepok Jidat',
+                   style: TextStyle(fontSize: 15, color: Colors.black),
+                 ),
+                 alignment: Alignment.center,
+                 width: 200.0,
+               ),
+             ],
+             ),
+           ),
+           Container(
+             color: Colors.white,
+             child: Text(
+               'Barcelona Feb 13, 2021',
+               style: TextStyle(fontSize: 15),
+             ),
+             height: 30.0,
+             alignment: Alignment.centerLeft,
+           ),
+           Container(
+             child: Row(children: [
+               Container(
+                 child: Image(image: NetworkImage('https://cdn.medcom.id/dynamic/content/2020/07/20/1166802/7QLnNcGsE0.jpg?w=700'),
+                 ),
+                 width: 160.0,
+                 //height: 80.0,
+               ),
+               Container(
+                //  decoration: BoxDecoration(
+                //    border: Border.all(color: Colors.green)
+                //    ),
+                   color : Colors.white,
+                 child : Text(
+                   'Pique Bilang Wasit Untungkan Madrid, Koeman  Tepok Jidat',
+                   style: TextStyle(fontSize: 15, color: Colors.black),
+                 ),
+                 alignment: Alignment.center,
+                 width: 200.0,
+               ),
+             ],
+             ),
+           ),
+           Container(
+             color: Colors.white,
+             child: Text(
+               'Barcelona Feb 13, 2021',
+               style: TextStyle(fontSize: 15),
+             ),
+             height: 30.0,
+             alignment: Alignment.centerLeft,
+           ),
+           Container(
+             child: Row(children: [
+               Container(
+                 child: Image(image: NetworkImage('https://cdn.medcom.id/dynamic/content/2020/07/20/1166802/7QLnNcGsE0.jpg?w=700'),
+                 ),
+                 width: 160.0,
+                 //height: 80.0,
+               ),
+               Container(
+                //  decoration: BoxDecoration(
+                //    border: Border.all(color: Colors.green)
+                //    ),
+                   color : Colors.white,
+                 child : Text(
+                   'Pique Bilang Wasit Untungkan Madrid, Koeman  Tepok Jidat',
+                   style: TextStyle(fontSize: 15, color: Colors.black),
+                 ),
+                 alignment: Alignment.center,
+                 width: 200.0,
+               ),
+             ],
+             ),
+           ),
+             Container(
+             color: Colors.white,
+             child: Text(
+               'Barcelona Feb 13, 2021',
+               style: TextStyle(fontSize: 15),
+             ),
+             height: 30.0,
+             alignment: Alignment.centerLeft,
+           ),
          ],
        ),
        ),
